6d5788f3adb0b233ac0f6db2e0dccd58e5ada48a
Greg Connell <greg.connell@ff0000.com>
2017-07-30 14:37:55 -0700

    Adds ./metamask-controller/package.json:  basis for TODO:  - crafting metamask-controller module  - eliminating extension/ui/view related dependencies, tests, & assets
    Adds ./metamask-controller/app.js:
     foundation for Application Interface, which will:
     - control instantiation of background/controller/state/api
     - make global.App available for window/node/etc contexts

---
+++ metamask-controller/app.js
@@ -0,0 +1,58 @@
+const EventEmitter = require('events')
+
+// dependencies
+let main = require('./scripts/background')
+
+// app version
+const packageJson = require('../../package.json')
+
+
+global.nodeVersion = false
+
+class App extends EventEmitter {
+	constructor(opts) {
+		super()
+		opts = opts || {}
+		if ('nodeVersion' in opts) {
+			nodeVersion = opts.nodeVersion
+		}
+		if (nodeVersion) {
+			require('longjohn') // long error-stack trace
+		}
+		this.main = main
+	}
+
+	init() {
+		console.log('App.init()')
+		// initialize state
+		this.main.init()
+
+		.then(() => {
+			//console.log('>>>', main.controller)
+			this.emit('ready', this.api)
+		})
+		.catch((err) => {
+			console.error(err)
+		})		
+	}
+
+	get controller() {
+		return main.controller
+	}
+
+	get api() {
+		return main.controller.getApi()
+	}
+
+}
+global.App = App
+
+
+
+
+
+
+
+
+
+
